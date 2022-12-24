d5b1d8d03f714c52c8ac3151f5d7bedcd985caf5
Ethan Lee <dalpong2014@gmail.com>
2022-12-23 19:14:09 -0500

    2nd commit

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const path = require("path");
+
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname + "/public")));
+
+//serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*,", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.use(require("./routes/record.js"));
 // get driver connection
 const dbo = require("./db/conn");
